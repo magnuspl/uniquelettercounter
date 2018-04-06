d5ad6b2c081774fd6c39e6c8c02b2c975404d522
magnuspl <magnuslangeland80@gmail.com>
2018-04-06 08:54:45 +0200

    remove old JSON function

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-/*function getJSON() {
-  document.getElementById("myList").innerHTML = "";
-  let url = document.getElementById('textbox_id').value
-  $.ajax({
-    type: "GET",
-    url: url,
-    dataType: 'json',
-    success: function(response){
-        countLetters(response)
-    }
-  });
-}
-
-function countLetters(response) {
-  var histogram = {};
-  var html = JSON.stringify(response)
-  getFrequency(html)
-  console.log(getFrequency(html))
-  for (var i = 0, len = html.length; i < len; i++) {
-      var letter = html[i];
-      for (var key in letter) {
-        if (letter.hasOwnProperty(key)) {
-          console.log(letter[key]);
-        }
-      }
-      histogram[letter] = (histogram[letter] || 0) + 1;
-      var node = document.createElement("LI");
-      var textnode = document.createTextNode((histogram[letter] || 0) + 1);
-      node.appendChild(textnode);
-      document.getElementById("myList").appendChild(node);
-  }
-} */
-
 function countUnqiueStringLetters() {
   // get input value
   let input = document.getElementById('textbox_string').value
